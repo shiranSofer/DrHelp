6098f37e0d0144e6a84108ade38bb9373d99f9c4
shiranSofer <shiran.soferg@gmail.com>
2019-05-07 23:13:18 +0300

    WeeklyShift Activity added

---
+++ app/src/main/java/com/example/shiran/drhelp/ui/WeeklyShiftActivity.java
@@ -0,0 +1,13 @@
+package com.example.shiran.drhelp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeeklyShiftActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekly_shift);
+    }
+}
