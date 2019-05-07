445d74f454f0054c45f83b9da1e1785396e328b4
shiranSofer <shiran.soferg@gmail.com>
2019-05-07 23:14:43 +0300

    WeeklyShift Activity added

--- app/src/main/java/com/example/shiran/drhelp/ui/WeeklyShiftActivity.java
+++ app/src/main/java/com/example/shiran/drhelp/ui/WeeklyShiftActivity.java
@@ -3,6 +3,8 @@ package com.example.shiran.drhelp.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.shiran.drhelp.R;
+
 public class WeeklyShiftActivity extends AppCompatActivity {
 
     @Override
